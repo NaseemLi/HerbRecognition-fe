dda786fd47d3f94f95dd8a652c580f6d350685b3
NaseemLi <naseem0922@proton.me>
2026-02-23 14:58:31 +0800

    feat:添加路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,12 +11,25 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     name: 'Home',
     component: () => import('@/views/Home.vue'),
+    meta: { requiresAuth: true },
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, _from, next) => {
+  const token = localStorage.getItem('token')
+
+  if (to.meta.requiresAuth && !token) {
+    next('/login')
+  } else if (to.path === '/login' && token) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
